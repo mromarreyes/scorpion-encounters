b9114efa8125e803166d2e011bff4787a07ba125
Omar Reyes <omar@petexec.net>
2019-02-06 14:38:44 -0700

    Added our first {get} /scorpion endpoint

--- back-end/handler.js
+++ back-end/handler.js
@@ -1,14 +1,10 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
+module.exports.scorpion = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      message: 'Encountered a scorpion!'
     }),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
